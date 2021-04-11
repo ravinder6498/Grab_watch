bd88ddedeb6a9f484d35a867d4c50c1fcf937f72
ravinder6498 <59883001+ravinder6498@users.noreply.github.com>
2021-04-11 12:10:05 -0400

    Add files via upload
    initial commit

---
+++ grab_Watch/grab_Watch/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  grab_Watch
+//
+//  Created by Ravinder on 2021-03-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sliderCollectionView: UICollectionView!
+    @IBOutlet weak var pageView: UIPageControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
